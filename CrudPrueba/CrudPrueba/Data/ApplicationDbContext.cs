d9a3a82a67f95e41763fcbb60c78e6bf0ba15a2b
Gerardo Lanza <glanza007@gmail.com>
2024-06-21 21:31:28 -0600

    Crud de prueba

---
+++ CrudPrueba/CrudPrueba/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CrudPrueba.Models;
+
+namespace CrudPrueba.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Empleado> Empleados { get; set; }
+    }
+}
---
+++ CrudPrueba/CrudPrueba/Models/Empleado.cs
@@ -0,0 +1,13 @@
+﻿namespace CrudPrueba.Models
+{
+    public class Empleado
+    {       
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = null!;
+
+        public DateTime FechaNacimiento { get; set; }
+
+
+    }
+}
